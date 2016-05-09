5698c615f778c9b4f3c2870254e023ef01bd15e2
qingyaoliu <qingyaoliu@yahoo.com>
2016-05-10 00:36:33 +0200

    Update index.js

--- BigBang/Meilenstein1/javascript/index.js
+++ BigBang/Meilenstein1/javascript/index.js
@@ -1,17 +1,17 @@
 function validateForm() {
 
-    var regexNames = /^[a-z??¨¹]+$/i;
+    var regexNames = /^[a-z??Â¨Â¹]+$/i;
 
     var vorname = document.getElementsByName('vorname')[0].value;
     var nachname = document.getElementsByName('name')[0].value;
     var verein = document.getElementsByName('verein')[0].value;
     var headcoach = document.getElementsByName('hcoach')[0].value;
-    var asscoach = document.getElementsByName('acoach')[0].value;
+    var assistantcoach = document.getElementsByName('acoach')[0].value;
 
-    if (!vorname.match(regexNames) || !name.match(regexNames) || !verein.match(regexNames) 
-        || !headcoach.match(regexNames) || !assistant.match(regexNames)) {
+    if (!vorname.match(regexNames) || !nachname.match(regexNames) || !verein.match(regexNames) 
+        || !headcoach.match(regexNames) || !assistantcoach.match(regexNames)) {
 
-        alert("Einige Eingaben sind fehlerhaft. Bitte ¨¹berpr¨¹fen Sie ihre Eingaben");
+        alert("Einige Eingaben sind fehlerhaft. Bitte Â¨Â¹berprÂ¨Â¹fen Sie ihre Eingaben");
         return false;
 
     }else{
