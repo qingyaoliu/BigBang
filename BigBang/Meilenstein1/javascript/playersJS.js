8969010dae497efa1cd2e96f698a343a179c9a7f
mei888 <1421858@stud.hs-mannheim.de>
2016-05-10 23:50:26 +0200

    Update playersJS.js

--- BigBang/Meilenstein1/javascript/playersJS.js
+++ BigBang/Meilenstein1/javascript/playersJS.js
@@ -1,31 +1,71 @@
 
-  var xhr=new XMLHttpRequest();
-	xhr.open('GET','http://server/api/players');
-	xhr.responseType='json';
-	xhr.onload=function(){
-	var data=xhr.response;
-	    if(data !== null){
-		console.log(data);
-	    }
-	};
-	xhr.send();
+     window.onload=function(){
+	var oDiv=document.getElementById('div1');
+	var aBtn=oDiv.getElementsByTagName('input');
+	var aDiv=oDiv.getElementsByTagName('div');
 	
-
- window.onload=function(){
-	var jsonResponse = xmlHttpReq.responseText;
-	var porvinceJsons = eval("("+ jsonResponse +")");
-	for(var i=0; i<porvinceJsons.length; i++){
-		alert("vorname="+porvinceJsons[i].vorname);
-		alert("naem="+porvinceJsons[i].name);
-		alert("verein="+porvinceJsons[i].verein);
-		alert("hcoach="+porvinceJsons[i].hcoach);
-		alert("aktiv="+porvinceJsons[i].aktiv);
-		alert("position="+porvinceJsons[i].position);
-		alert("number="+porvinceJsons[i].number);
-		alert("jahr="+porvinceJsons[i].jahr);
-		alert("favorit="+porvinceJsons[i].favorit);
+	for(var i=0; i<aBtn.length;i++){
+		aBtn[i].index=i;
+		aBtn[i].onclick=function(){
+			for(var i=0; i<aBtn.length; i++){
+				aBtn[i].className='';
+				aDiv[i].style.display='none';
+			}
+			this.className='active';
+			aDiv[this.index].style.display='block';
+		};
 	}
-  }
+     };
 
 
- 
+
+window.onload=function(){
+	var oBtn=document.getElementById('btn1');
+	oBtn.onclick=function(){
+	ajax(''http://139.59.134.26/api/players'',function(str){
+		alert(str);
+	});
+     };
+ };
+
+function ajax(url, fnSucc, fnFaild){
+	 if(window.XMLHttpRequest){
+		var oAjax=new XMLHttpRequest();
+	}else{
+		var oAjax=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+
+	oAjax.open('GET', 'http://139.59.134.26/api/players?t='+new Date().getTime(),true);
+	oAjax.send();
+	oAjax.onreadystatechange=function(){
+	if(oAjax.readState==4){
+		if(oAjax.status==200){
+			fnSucc(oAjax.responseText);
+		}else{
+			if(fnFaild){
+		  	fnFaild(oAjax.status);
+		}
+     	}
+     };
+};
+
+
+  window.onload=function(){
+	var oBtn=document.getElementById('btn1');
+	var oTr=document.getElementById('tr1');
+
+	oBtn.onclick=function(){
+	    ajax('http://139.59.134.26/api/players?t='+new Date().getTime, function(str){
+		var arr=eval(str);
+		for(var i=0; i<arr.length;i++){
+
+		var oTd=document.createElement('tr');
+		oTd.innerHTML='<td>'arr[i].vorname+'</td>'+'<td>'+arr[i].name+'</td>'
+'<td>'arr[i].verein+'</td>'+'<td>'+arr[i].hcoach+'</td>';
+		oTr.appendChild(oTd);
+		}
+	    }, function(){
+		alert('scheitert');
+	    });
+	};
+  };
