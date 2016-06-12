a47c935be14b89e44cb5ea85672e2e46eb10678c
mei888 <1421858@stud.hs-mannheim.de>
2016-06-12 09:21:07 +0200

    server changed

--- BigBang/Meilenstein4/server.js
+++ BigBang/Meilenstein4/server.js
@@ -1,70 +1,37 @@
 var   express = require('express');
 var   app = express();
-var   http = require('http').Server(app);
-var   io = require('socket.io').http;
-
-var   ipAddr = '127.0.0.1';
+var   server = require('http').createServer(app);
+var   io = require('socket.io').listen(server);
+var   host = '127.0.0.1';
 var   port = '8080';
 
+server.listen(port);
 
+	// statische Dateien ausliefern
+	app.use(express.static(__dirname + '/public'));
 
 	
 app.get('/', (req, res)=>{
 	// chat.html wird ausgegeben
-	res.sendfile(__dirname + '/public/chat.html');
+	res.sendFile(__dirname + '/public/chat.html');
 });
 
 
 
-io.sockets.on('connection', (socket)=>{
+io.sockets.on('connection', function (socket) {
 	// der Client ist verbunden
-	socket.emit('Willkommen, bitte gebe deinen Usernamen ein!');
-	
-	var client={
-    socket:socket,
-    name:false
-	}
-	
-	socket.on('chat message',(msg)=>{
-        console.log('chat message:'+msg);
-        var obj={time:getTime()}; 
-         
-       
-        if(!client.name){
-            client.name=msg;
-            obj['text']=client.name;
-            obj['author']='Sys';
-            obj['type']='welcome';
-            socket.name=client.name; 
-            if(!onlineUsers.hasOwnProperty(client.name)){
-                onlineUsers[client.name]=client.name;
-            }
-			socket.emit('system',obj); 
-		}else{
-			obj['text']=msg;
-            obj['author']=client.name;
-            obj['type']='message';
-            console.log(client.name+' say:'+msg);
-			//die Nachricht, die von sich selbst geschrieben wurde,ausdrücken
-			socket.emit('chat message',obj);
-		}
-	});
+	socket.emit('chat', { zeit: new Date(), text: 'Du bist nun mit dem Server verbunden!' });
 	// wenn ein Benutzer einen Text senden
-	socket.on('chat', (data)=>{
+	socket.on('chat', function (data) {
 		// so wird dieser Text an alle anderen Benutzer gesendet
-		io.sockets.emit('chat', { text: "Willkommen ", name: data.name});
+		io.sockets.emit('chat', { zeit: new Date(), name: data.name || 'Anonym', text: data.text });
 	});
-	
 });
 
 
 
 // Portnummer in die Konsole schreiben
-http.listen(port,()=>{
+
 	console.log('Der Server läuft nun unter http://127.0.0.1:' + port + '/');
-});
+
 	
-var getTime=()=>{
-  var date = new Date();
-  return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-}
