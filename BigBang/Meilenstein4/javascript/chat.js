4fbf0a05881a67eb7f289d9df8c00f49ad1b49f0
qingyaoliu <qingyaoliu@yahoo.com>
2016-06-13 18:19:31 +0200

    Update chat.js

--- BigBang/Meilenstein4/javascript/chat.js
+++ BigBang/Meilenstein4/javascript/chat.js
@@ -3,13 +3,13 @@ const socket = io.connect('http://localhost:8080');
     socket.emit('addme', prompt('Willkommen, bitte gebe deinen Username ein')); 
   });
   socket.on('chat',(username, data)=>{ 
-    let p = document.createElement('p'); 
+    var p = document.createElement('p'); 
     p.innerHTML = username + ': ' + data;
     document.getElementById('output').appendChild(p); 
   });
   window.addEventListener('load',()=> { 
-    document.getElementById('sendtext').addEventListener('click',()=> {
-        let text = document.getElementById('data').value; 
+    document.getElementById('sendtext').addEventListener('click',function() {
+        var text = document.getElementById('data').value; 
         socket.emit('sendchat', text);
     }, false); 
   }, false);
